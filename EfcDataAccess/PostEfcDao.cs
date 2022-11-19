ea7be40c143082d17eaa1ed0981d0b97b334a8e5
Natalia <koziarkka@gmail.com>
2022-11-19 10:26:44 +0100

    Assignment third done + file data still here

---
+++ EfcDataAccess/PostEfcDao.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.CompilerServices;
+using Application.DaoInterfaces;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace EfcDataAccess;
+
+public class PostEfcDao : IPostDao
+{
+    private readonly PostContext context;
+
+    public PostEfcDao(PostContext context)
+    {
+        this.context = context;
+    }
+
+    public async Task<Post> CreateAsync(Post post)
+    {
+        EntityEntry<Post> newPost = await context.Posts.AddAsync(post);
+        await context.SaveChangesAsync();
+        return newPost.Entity;
+    }
+
+    public async Task<IEnumerable<Post>> GetAsync()
+    {
+        IQueryable<Post> postsQuery = context.Posts.Include(post => post.Owner).AsQueryable();
+        List<Post> result = await postsQuery.ToListAsync();
+        return result;
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        Post? post = await GetByIdAsync(id);
+        if (post == null)
+        {
+            throw new Exception($"Post with id {id} not found");
+        }
+
+        context.Posts.Remove(post);
+        await context.SaveChangesAsync();
+
+    }
+
+    public async Task<Post> GetByIdAsync(int id)
+    {
+        Post? post = await context.Posts.AsNoTracking().Include(post => post.Owner).SingleOrDefaultAsync(post => post.id == id);
+        return post;
+    }
+}
---
+++ EfcDataAccess/UserEfcDao.cs
@@ -0,0 +1,43 @@
+﻿using Application.DaoInterfaces;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace EfcDataAccess;
+
+public class UserEfcDao : IUserDao
+{
+    private readonly PostContext context;
+
+    public UserEfcDao(PostContext context)
+    {
+        this.context = context;
+    }
+
+    public async Task<User> CreateAsync(User user)
+    {
+        EntityEntry<User> newUser = await context.Users.AddAsync(user);
+        await context.SaveChangesAsync();
+        return newUser.Entity;
+    }
+
+    public async Task<User?> GetByUsernameAsync(string userName)
+    {
+        User? existing = await context.Users.FirstOrDefaultAsync(u => u.UserName.ToLower().Equals(userName.ToLower()));
+        return existing;
+
+    }
+
+    public async Task<IEnumerable<User>> GetAsync()
+    {
+        IQueryable<User> usersQuery = context.Users.AsQueryable();
+        IEnumerable<User> result = await usersQuery.ToListAsync();
+        return result;
+    }
+
+    public async Task<User?> GetByIdAsync(int id)
+    {
+        User? existing = await context.Users.FindAsync(id);
+        return existing;
+    }
+}
